0673580e8186d7a22e149cc4d820934becc67489
Vesa Malaska <vesa.malaska@gmail.com>
2019-04-25 12:13:37 +0300

    all files

--- application/controllers/Newsletter.php
+++ application/controllers/Newsletter.php
@@ -17,24 +17,75 @@ class Newsletter extends MY_Controller {
     // page title
     $data['page_title'] = 'Uutiskirjeiden l&auml;hett&auml;minen';
 
+		// load template library for handling the view
+		$this->load->library('template');
+
+		// load newsletter view in default template
+		$this->template->load('default','newsletter' , $data);
+  }
+
+  public function send() {
+
+    /***********************************************************/
+    /*     FILE UPLOADS
+    /***********************************************************/
+
+    // config for file updloads
+    $config['upload_path'] = './tmp/';
+    $config['allowed_types'] = 'txt';
+    $config['file_name'] = 'email_list.txt';
+    $this->load->library('upload', $config);
+
+    // Upload email list file
+    $this->upload->do_upload("email_list");
+
+    // Override file name for the other file upload
+    $config['file_name'] = 'email_template.html';
+    $this->upload->initialize($config);
+
+    // Upload email list file
+    $this->upload->do_upload("email_template");
+
     // Load the email config model
 		$this->load->model('email_config_model');
+
+    /***********************************************************/
+    /*     EMAIL SETTINGS AND SEND
+    /***********************************************************/
 
     // Get the email config from the database
     $email_config = $this->email_config_model->get_email_config();
 
-    // put the email config data to data array
-    $data['email_config_set_name'] = $email_config['config_set_name'];
-    $data['email_protocol'] = $email_config['protocol'];
-    $data['email_smtp_host'] = $email_config['smtp_host'];
-    $data['email_smtp_user'] = $email_config['smtp_user'];
-    $data['email_smtp_pass'] = $email_config['smtp_pass'];
-    $data['email_smtp_port'] = $email_config['smtp_port'];
-    $data['email_smtp_crypto'] = $email_config['smtp_crypto'];
-    $data['email_mailtype'] = $email_config['mailtype'];
-    $data['email_charset'] = $email_config['charset'];
-    $data['email_crlf'] = $email_config['crlf'];
-    $data['email_newline'] = $email_config['newline'];
+    // Load email libary
+    $this->load->library('email');
+
+    // Set the SMTP configuration for email to be sent
+    $this->email->initialize($email_config);
+
+    // Get the recipient email addresses from the email_list file
+    $recipients = "";
+
+    // read file row by row
+    while(! feof("/tmp/email_list.txt")) {
+      $recipients = fgets("/tmp/email_list.txt").", ";
+    }
+
+    // set parameters for email to be sent
+    $this->email->from('myynti@toimistopoika1.fi', 'Toimistopoika');
+    $this->email->to('palvelu@toimistopoika.fi');
+    $this->email->bcc($recipients);
+
+    $this->email->subject('Windows 7 tuki p&auml;&auml;ttyy pian!');
+    $this->email->message(readfile("/tmp/email_template.html"));
+
+    $this->email->send();
+
+    /***********************************************************/
+    /*     LOADING VIEW
+    /***********************************************************/
+
+    // page title
+    $data['page_title'] = 'Uutiskirjeiden l&auml;hett&auml;minen';
 
 		// load template library for handling the view
 		$this->load->library('template');
--- application/views/newsletter.php
+++ application/views/newsletter.php
@@ -1,1 +1,37 @@
-<?php echo $email_config_set_name; ?>
+<?php
+
+// echo $email_config_set_name;
+
+// Form attributes
+//$form_attributes = array('enctype' => 'multipart/form-data');
+$email_list = array(
+  'name'      => 'email_list',
+  'id'        => 'email_list'
+);
+$email_template = array(
+  'name'      => 'email_template',
+  'id'        => 'email_template'
+);
+
+
+echo form_open_multipart('newsletter/send');
+?>
+<p>
+Email list:<br>
+<?php
+echo form_upload($email_list);
+?>
+
+</p>
+<p>
+email template (HTML):<br>
+<?php
+echo form_upload($email_template);
+?>
+</p>
+<p>
+<?php
+echo form_submit('newsletter_submit', 'Lähetä!');
+echo form_close();
+?>
+</p>
